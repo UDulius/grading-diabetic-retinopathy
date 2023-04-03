ef9c136dbfe3be9006f11a8879d084151b0d211e
UDulius <u.dulius@icloud.com>
2023-04-03 17:41:27 +0100

    moving of images, splitting train and validation

--- grading-diabetic-retinopathy/grading_diabetic_retinopathy.py
+++ grading-diabetic-retinopathy/grading_diabetic_retinopathy.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import os, shutil
+import cv2
 
-#Assign training and testing images for exploratory data analysis
+image_size = (512, 512)
 
 #Create a Pandas dataframe for training data for exploratory data analysis
 df = pd.read_csv(r'../images/trainLabels.csv')
@@ -21,8 +22,29 @@ print(f"There are {df.duplicated().sum()} repeated rows")
 print(df.isnull().sum())
 
 #Visualisation of quantity of images related to their grade of retinopathy
-#distribution = sns.countplot(x=df['level'])
-#plt.show()
+distribution = sns.countplot(x=df['level'])
+plt.show()
+
+#
+
+#Assign training/testing to np.arrays
+def train_load(dir):
+    temp = []
+    i = 0
+    for img in os.listdir(dir):
+        try:
+            arr = cv2.imread(os.path.join(dir, img), cv2.IMREAD_GRAYSCALE)
+            resize = cv2.resize(arr, (image_size))
+            temp.append([resize, df.loc[i, 'level']])
+        except Exception as exception:
+            print(exception)
+    return np.array(temp)
+
+train = train_load(r'../images/train')
+
+print(train.shape())
+
+
 
 
 
